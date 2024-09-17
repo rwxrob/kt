dd18f3ac4dd3ba20ac36e4449129b24fcbffdd6a
Rob Muhlestein <rob@rwx.gg>
2024-09-17 15:22:52 -0400

    add completion

--- cmd/kt/main.go
+++ cmd/kt/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"os"
+
+	kt "github.com/rwxrob/kt/pkg/cmd"
 	"github.com/spf13/cobra"
 )
 
-var Cmd = &cobra.Command{
-	Use:     `kt`,
-	Short:   `Kubernetes tools`,
-	Version: `v0.0.1`, // TODO change this to something triggered by tag
+var completionCmd = &cobra.Command{
+	Use:       "completion (bash|zsh|fish|pwsh)",
+	Short:     "Generate completion script",
+	Args:      cobra.MatchAll(cobra.ExactValidArgs(1)),
+	ValidArgs: []string{`bash`, `zsh`, `fish`, `pwsh`},
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			kt.Cmd.GenBashCompletion(os.Stdout)
+		case "zsh":
+			kt.Cmd.GenZshCompletion(os.Stdout)
+		case "fish":
+			kt.Cmd.GenFishCompletion(os.Stdout, true)
+		case "pwsh":
+			kt.Cmd.GenPowerShellCompletion(os.Stdout)
+		}
+	},
 }
 
 func main() {
-	Cmd.Execute()
+	kt.Cmd.AddCommand(completionCmd)
+	kt.Cmd.Execute()
 }
---
+++ pkg/cmd/kt.go
@@ -0,0 +1,9 @@
+package kt
+
+import "github.com/spf13/cobra"
+
+var Cmd = &cobra.Command{
+	Use:     `kt`,
+	Short:   `Kubernetes tools`,
+	Version: `v0.0.1`, // TODO change this to something triggered by tag
+}
