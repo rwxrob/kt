311e80d2057ca602aaa0a25bbb8eef1e3a622bad
Rob Muhlestein <rob@rwx.gg>
2024-09-16 20:48:39 -0400

    add better helm version lookup errors

--- helm/helm.go
+++ helm/helm.go
@@ -3,8 +3,7 @@ package helm
 import (
 	"encoding/json"
 	"fmt"
-
-	"github.com/rwxrob/kutil/cli"
+	"os/exec"
 )
 
 // Only stuff we care about
@@ -17,12 +16,18 @@ type Chart struct {
 // AppVersionFor uses the helm command to lookup the app_version for
 // the given chart version.
 func AppVersionFor(chart, chartver string) (string, error) {
+	var err error
 
-	jsonData := cli.Out(
+	cmd := exec.Command(
 		`helm`, `search`, `repo`, chart, `-o`, `json`, `--version`, chartver)
+	jsonData, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("helm search command failed: %w", err)
+	}
 
 	var charts []Chart
-	if err := json.Unmarshal([]byte(jsonData), &charts); err != nil {
+	err = json.Unmarshal([]byte(jsonData), &charts)
+	if err != nil {
 		return "", err
 	}
 
@@ -33,4 +38,29 @@ func AppVersionFor(chart, chartver string) (string, error) {
 	}
 
 	return "", fmt.Errorf(`app version not found for chart version %q`, chartver)
+}
+
+// ChartVersionFor uses the helm command to lookup the version for the
+// given app_version.
+func ChartVersionFor(chart, appver string) (string, error) {
+
+	cmd := exec.Command(
+		`helm`, `search`, `repo`, chart, `-o`, `json`, `--versions`)
+	jsonData, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("helm search command failed: %w", err)
+	}
+
+	var charts []Chart
+	if err := json.Unmarshal([]byte(jsonData), &charts); err != nil {
+		return "", err
+	}
+
+	for _, chart := range charts {
+		if chart.AppVersion == appver {
+			return chart.ChartVersion, nil
+		}
+	}
+
+	return "", fmt.Errorf(`chart version not found for app version %q`, appver)
 }
--- helm/helm_test.go
+++ helm/helm_test.go
@@ -15,3 +15,37 @@ func ExampleAppVersionFor() {
 	// Output:
 	// 2.11.1
 }
+
+func ExampleAppVersionFor_badversion() {
+	version, err := helm.AppVersionFor(`harbor/harbor`, `xxx`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(version)
+	// Output:
+	// helm search command failed: exit status 1
+
+}
+
+func ExampleChartVersionFor() {
+	version, err := helm.ChartVersionFor(`harbor/harbor`, `2.11.1`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(version)
+	// Output:
+	// 1.15.1
+}
+
+func ExampleChartVersionFor_badversion() {
+	version, err := helm.ChartVersionFor(`harbor/harbor`, `xxx`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(version)
+	// Output:
+	// chart version not found for app version "xxx"
+
+}
