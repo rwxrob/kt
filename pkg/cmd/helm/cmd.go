0881834e2d477fdca267338df759118f2f9f1342
Rob Muhlestein <rob@rwx.gg>
2024-09-28 17:00:35 -0400

    add version to helm subtree

--- pkg/cmd/helm/cmd.go
+++ pkg/cmd/helm/cmd.go
@@ -3,8 +3,9 @@ package helm
 import "github.com/spf13/cobra"
 
 var Cmd = &cobra.Command{
-	Use:   `helm COMMAND`,
-	Short: `Collection of Helm tools`,
+	Use:     `helm COMMAND`,
+	Short:   `Collection of Helm tools`,
+	Version: `v0.0.1`,
 }
 
 func init() {
